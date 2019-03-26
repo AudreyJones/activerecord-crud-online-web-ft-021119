f6dc7c38b86b6dc7dbffd6878fc5ec8d3e759cf4
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-26 15:34:21 +0000

    Automatically backed up by Learn

--- db/migrate/20190326152822_create_movies.rb
+++ db/migrate/20190326152822_create_movies.rb
@@ -1,16 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
-  create_table :movies do |t|
-    t.string :title
-    t.integer :release_date
-    t.string :director
-    t.string :lead
-    t.boolean :in_theaters
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
   end
-  # def change
-  #   add_column :movies, :title, :string
-  #   add_column :movies, :release_date, :integer
-  #   add_column :movies, :director, :string
-  #   add_column :movies, :lead, :string
-  #   add_column :movies, :in_theaters, :boolean
-  # end
 end
